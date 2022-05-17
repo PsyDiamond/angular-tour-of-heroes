00739735eb6c35d22b7f1874204ef686e403fbb4
Lex Talionis <lextalionis1488@gmail.com>
2022-05-17 11:32:41 +0100

    Step 5 Add Navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
     this.messageService.add('HeroService: featched heroes');
     return heroes;
   }
+
+  getHero(id: Number): Observable<Hero> {
+    const hero = HEROES.find(x => x.id === id)!;
+    this.messageService.add(`HeroService: featched hero id=${id}`);
+    return of(hero);
+  }
 }
